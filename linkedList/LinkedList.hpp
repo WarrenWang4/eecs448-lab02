bc453f219d92af78427dd4fd2f2ee1e6f763e2d2
Fnu Warren <warrenw@ku.edu>
2020-02-03 15:46:31 -0600

    Fixed bug in search method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,9 +29,6 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO
-		Fix this method
-	*/
 	return(m_size);
 }
 
@@ -41,14 +38,11 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
 	while( temp != nullptr )
 	{
 		if (temp == value)
 		{
-			isFound = true
+			isFound = true;
 		}
 		temp = temp->getNext();
 	}
